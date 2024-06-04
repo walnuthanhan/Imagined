7d92c8f32e7df2c241aede0116cc8b6edc4891d4
hantrinh13 <trinhgiahan1308@gmail.com>
2024-06-03 23:57:57 -0500

    Add source folder

---
+++ src/main/java/com/example/imagined/ImaginedApplication.java
@@ -0,0 +1,14 @@
+package com.example.imagined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaginedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaginedApplication.class, args);
+	}
+	/*Add comment to test*/
+
+}
---
+++ src/test/java/com/example/imagined/ImaginedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaginedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
